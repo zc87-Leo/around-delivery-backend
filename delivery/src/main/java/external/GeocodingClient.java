be6925657f01be90427a32975a0f64295fd5a2d8
Sai Chen <chensai0811@gmail.com>
2020-07-04 03:52:51 -0400

    initial java and tomcat with google map api

---
+++ delivery/src/main/java/external/GeocodingClient.java
@@ -0,0 +1,11 @@
+package external;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.model.GeocodingResult;
+
+public class GeocodingClient {
+	
+}
