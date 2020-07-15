f40d5b329e662c8a32c1f9e1a41c585bed4f4a03
Yi Jiang <jiangyi2010@hotmail.com>
2020-07-15 12:10:21 -0600

    adding address auto complete

--- external/SmartyStreetsClient.java
+++ external/SmartyStreetsClient.java
@@ -1,17 +1,16 @@
 package external;
 
 import com.smartystreets.api.StaticCredentials;
-import com.smartystreets.api.SharedCredentials;
 import com.smartystreets.api.exceptions.BatchFullException;
 import com.smartystreets.api.exceptions.SmartyException;
+import com.smartystreets.api.us_autocomplete.Suggestion;
 import com.smartystreets.api.us_street.*;
 import com.smartystreets.api.ClientBuilder;
 
 import java.io.IOException;
-import java.net.Proxy;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
+
 
 public class SmartyStreetsClient {
 	// private static final String AUTH_ID = "f748fd62-a78a-a8a9-57e1-39d9a59bc6e3";
@@ -25,8 +24,8 @@ public class SmartyStreetsClient {
 	public static List<List<Candidate>> isValidAddress(String senderAddr, String receiverAddr) {
 		List<List<Candidate>> result = new ArrayList<>();
 
-		String authId = "f748fd62-a78a-a8a9-57e1-39d9a59bc6e3";
-		String authToken = "EKqmZ8JEx0BO7FVbiwbp";
+		String authId = "0013481a-b3e8-13b8-7efe-e4ca14d85bce";
+		String authToken = "bD7m8jU06oq5ZQaJ3LNR";
 		StaticCredentials credentials = new StaticCredentials(authId, authToken);
 
 		Client client = new ClientBuilder(credentials).buildUsStreetApiClient();
@@ -50,7 +49,7 @@ public class SmartyStreetsClient {
 			ex.printStackTrace();
 		}
 
-		Vector<Lookup> lookups = batch.getAllLookups();
+		List<Lookup> lookups = batch.getAllLookups();
 
 		for (int i = 0; i < batch.size(); i++) {
 			ArrayList<Candidate> candidates = lookups.get(i).getResult();
@@ -86,6 +85,48 @@ public class SmartyStreetsClient {
 			}
 			System.out.println();
 
+		}
+		return result;
+	}
+	
+	public static List<Candidate> getValidAddresses(Suggestion[] suggestions) {
+		List<Candidate> result = new ArrayList<>();
+		String authId = "0013481a-b3e8-13b8-7efe-e4ca14d85bce";
+		String authToken = "bD7m8jU06oq5ZQaJ3LNR";
+		StaticCredentials credentials = new StaticCredentials(authId, authToken);
+
+		Client client = new ClientBuilder(credentials).buildUsStreetApiClient();
+		Batch batch = new Batch();
+		
+		try {
+			for (Suggestion suggestion : suggestions) {
+				Lookup lookup = new Lookup();
+				lookup.setStreet(suggestion.getStreetLine());
+				lookup.setCity(suggestion.getCity());
+				lookup.setState(suggestion.getState());
+				lookup.setMaxCandidates(1);
+		        lookup.setMatch(MatchType.INVALID);
+				batch.add(lookup);
+			}
+			client.send(batch);
+		} catch (BatchFullException ex) {
+			System.out.println("Oops! Batch was already full.");
+		} catch (SmartyException ex) {
+			System.out.println(ex.getMessage());
+			ex.printStackTrace();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		List<Lookup> lookups = batch.getAllLookups();
+		
+		
+		for (int i = 0; i < lookups.size(); i++) {
+			List<Candidate> candidates = lookups.get(i).getResult();
+			//skip it if no candidate or the zipcode of the candidate is null
+			if (candidates == null || candidates.size() == 0 || candidates.get(0).getComponents().getZipCode() == null) {
+				continue;
+			}
+			result.add(candidates.get(0));
 		}
 		return result;
 	}
