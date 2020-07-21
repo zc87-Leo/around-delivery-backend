d499812e0c600c0aa8af7b99416de954077a9027
Bingqi Zhou <biz024@ucsd.edu>
2020-07-21 10:24:10 -0700

    Added active-orders servlet

--- db/MySQLConnection.java
+++ db/MySQLConnection.java
@@ -245,6 +245,48 @@ public class MySQLConnection {
 		return items;
 	}
 	
+	public List<String> getActive(String user_id) {
+		if (conn == null) {
+			System.err.println("DB connection failed");
+			return new ArrayList<String>();
+		}
+		List<String> items = new ArrayList<String>();
+		try {
+			String sql = "SELECT o.order_id, o.tracking_id, c.first_name, c.last_name, c.address, t.status, "
+					+ "t.created_at, t.delivered_at "
+					+ "FROM users u, orders o, contact c, tracking t "
+					+ "WHERE u.user_id = ? "
+					+ 	"AND t.status = 'active' "
+					+ 	"AND u.user_id = o.user_id " 
+					+	"AND o.recipient_id = c.contact_id "
+					+ 	"AND o.tracking_id = t.tracking_id";
+			System.out.println(sql);
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setString(1, user_id);
+			ResultSet rs = statement.executeQuery();
+			while (rs.next()) {
+				String order_id = rs.getString("order_id");
+				items.add(order_id);
+				String status = rs.getString("status");
+				items.add(status);
+				String name = rs.getString("first_name") + " " + rs.getString("last_name");
+				items.add(name);
+				String address = rs.getString("address");
+				items.add(address);
+				String delivered_at = rs.getString("delivered_at");
+				items.add(delivered_at);
+				String created_at = rs.getString("created_at");
+				items.add(created_at);
+				String tracking_id = rs.getString("tracking_id");
+				items.add(tracking_id);
+			}
+		}
+		catch (SQLException e){
+			e.printStackTrace();
+		}
+		return items;
+	}
+	
 	public List<String> getDetail(String order_id) {
 		if (conn == null) {
 			System.err.println("DB connection failed");
---
+++ rpc/ActiveOrders.java
@@ -0,0 +1,85 @@
+package rpc;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import db.MySQLConnection;
+
+/**
+ * Servlet implementation class ActiveOrders
+ */
+public class ActiveOrders extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ActiveOrders() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// read content in the request 
+		JSONObject input = RpcHelper.readJSONObject(request);
+		String userId = input.getString("user_id");
+		
+		// use the content to communicate with database
+		MySQLConnection connection = new MySQLConnection();
+		JSONArray arr = new JSONArray();
+		List<String> items = connection.getActive(userId);
+		if (items.size() == 0) {
+			arr.put(new JSONObject().put("alert", "order id not valid"));
+		}
+		else {
+			try {
+				while (!items.isEmpty()) {
+					// aggregate result of one history item
+					JSONObject cur = new JSONObject();
+					List<String> cols = new ArrayList<String>();
+					cols.add("Order ID");
+					cols.add("Order Status");
+					cols.add("Recipient");
+					cols.add("Delivery Address");
+					cols.add("Delivery Time");
+					cols.add("Order Date");
+					cols.add("Tracking ID");
+					for (int i = 0; i < 7; i++) {
+						cur.put(cols.get(i), items.get(0));
+						items.remove(0);
+					}
+					// put the current array json object into the json array
+					arr.put(cur);
+				}
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		// close database connection and return response object
+		connection.close();
+		RpcHelper.writeJsonArray(response, arr);
+	}
+
+}
